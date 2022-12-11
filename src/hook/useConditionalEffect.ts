2321a39caf6db43f297fc71e8ac28a55a58e380f
Amir <kazemi.amir68@gmail.com>
2022-12-11 15:29:17 +0100

    first commit

---
+++ src/hook/useConditionalEffect.ts
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from "react";
+
+interface EffectFunctionResult {
+  finished: boolean;
+  cleanup: Cleanup;
+}
+
+interface Props {
+  effectFunction: () => boolean | EffectFunctionResult;
+  dependencies?: React.DependencyList;
+}
+
+type Cleanup = () => void;
+
+const useConditionalEffect = ({ effectFunction, dependencies }: Props) => {
+  const flag = useRef<boolean>(false);
+  const [cleanup, setCleanup] = useState<Cleanup | null>(null);
+
+  useEffect(() => {
+    if (!flag.current) {
+      const result = effectFunction();
+      if (typeof result === "boolean") {
+        flag.current = result;
+      } else {
+        flag.current = result.finished;
+
+        if (!flag.current) {
+          return result.cleanup;
+        } else {
+          setCleanup(result.cleanup);
+        }
+      }
+    }
+  }, dependencies);
+
+  useEffect(() => {
+    if (cleanup) {
+      return cleanup;
+    }
+  }, [cleanup]);
+};
+
+export default useConditionalEffect;
